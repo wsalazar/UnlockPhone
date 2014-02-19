79bf27578cd2e59d2221dfa92f362035b5ec8d4a
Will Salazar <was@williamsalazar.com>
2014-02-18 21:24:22 -0500

    PayPal classes.

--- src/Unlock/FormBundle/Services/RestService.php
+++ src/Unlock/FormBundle/Services/RestService.php
@@ -2,19 +2,27 @@
 
 namespace Unlock\FormBundle\Services;
 
+use PayPal\Auth\OAuthTokenCredential;
+
+use PayPal\Auth\Openid\PPOpenIdSession;// as OpenIdSession;
+
 class RestService
 {
 
 	const PAYPAL_SANDBOX_URL = 'https://api.sandbox.paypal.com/v1/';
 	const TEST_ACCOUNT = 'will.a.salazar-facilitator@gmail.com';
-	const CLIENT_ID = 'AVKZiRAC2C-fS-yDLWkrzWL3N-0M0pxV8_31Ag0jVEVSckexk2wO0C5B3bJ1';
-	const SECRET = 'EIuSqhBjIdiADvhFMA1KPE6Qq6MOFBvkVH5ACI1s5OBECyUM2-Wg10EKFTg6';
+	//const CLIENT_ID = 'AVKZiRAC2C-fS-yDLWkrzWL3N-0M0pxV8_31Ag0jVEVSckexk2wO0C5B3bJ1';
+	const CLIENT_ID = 'AakKmRAUxxENYtDWMvc05LEylx9Mw6GSCkx7zpKnfLx-JyOJnQySEnXlnwhr';
+	//const SECRET = 'EIuSqhBjIdiADvhFMA1KPE6Qq6MOFBvkVH5ACI1s5OBECyUM2-Wg10EKFTg6';
+	const SECRET = 'EH3SoxBOTKLyeMbxOU9nWh7sFeQTClCoYk_VVSFwYVKnKIo1fKD3TzJRtLKM';
 
 	private $token;
 
 	public function hasAccess(){
+		$oAuth = new OAuthTokenCredential(RestService::CLIENT_ID, RestService::SECRET);
+		return $oAuth->getAccessToken(array('mode' => 'sandbox'));
+		/*
 		$init = curl_init();
-
 		curl_setopt($init, CURLOPT_URL, RestService::PAYPAL_SANDBOX_URL . "oauth2/token");
 		curl_setopt($init, CURLOPT_HEADER, false);
 		curl_setopt($init, CURLOPT_SSL_VERIFYPEER, false);
@@ -24,19 +32,24 @@ class RestService
 		curl_setopt($init, CURLOPT_POSTFIELDS, "grant_type=client_credentials");
 		$result = curl_exec($init);
 		curl_close($init);
-		/*
-		$curl = 'curl -v ' . RestService::PAYPAL_SANDBOX_URL . 'oauth2/token -H "Accept: application/json" -H "Accept-Language: en_US" -u "'.
-					RestService::CLIENT_ID . ':'. RestService::SECRET . '" -d "grant_type=client_credentials"';
-*/
+
 		return $result;
-		//return shell_exec($curl);
+		*/
+
 	}
 
-	public function setToken($tokenHandler){
-		$accessToken = json_decode($tokenHandler);
-		$this->token = $accessToken->access_token;
+	public function grantAccess($apiContext){
+		//$apiContext = new PPApiContext(array('mode' => 'sandbox'));
+		$scope = array('openid', 'email');
+		//echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+		$redirectUri = 'http://'.$_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+		return PPOpenIdSession::getAuthorizationUrl($redirectUri, $scope, RestService::CLIENT_ID, $apiContext);
 	}
 
+	public function setToken($token){
+		$this->token = $token;
+	}
+/*
 	public function getToken(){
 		return $this->token;
 	}
@@ -46,7 +59,6 @@ class RestService
 		$data = array('intent' 	=> 'sale', 'redirect_urls' => array('return_url' => 'http://unlock.lcl/app_dev.php/unlock',
     					'cancel_url' => 'http://unlock.lcl/app_dev.php/unlock'),'payer' => array('payment_method' => 'paypal'),
 						'transactions' => array( array('amount' => array('total'=>'125','currency' => 'USD') ) ) );
-		//$data = '{"intent":"sale","payer":{"payment_method":"paypal"},"transactions":[{"amount":{"total":"7.47","currency":"USD"}}]}';
 		$data = json_encode($data);
 		curl_setopt($init, CURLOPT_URL, RestService::PAYPAL_SANDBOX_URL ."payments/payment");
 		curl_setopt($init, CURLOPT_POST, true);
@@ -59,28 +71,8 @@ class RestService
 			);
 		$result = curl_exec($init);
 		var_dump( $result );
-		//$result = json_decode($result);
 		curl_close( $init );
 		return $result;
-/*
-		$json = array('intent' 	=> 'sale', 'payer' => array('payment_method' => 'paypal'),
-				'transactions' => array( array('amount' => array('total'=>'125','currency' => 'USD') ) ) );
-*/		
-/*
-		$curl = "curl -v " . RestService::PAYPAL_SANDBOX_URL ."payments/payment -H 'Content-Type: application/json' 
-		-H 'Authorization: Bearer {" . $this->getToken() . "}' -d '" . $json . "'";
-		//.{'intent':'sale','payer':{'payment_method':'paypal'},'transactions':[{'amount':{'total':'7.47','currency':'USD'}}]}'<br /><br />";
-		echo $curl . '<br /><br />';
-		//echo $json;
-		echo "curl -v https://api.sandbox.paypal.com/v1/payments/payment -H 'Content-Type: application/json' -H 'Authorization: Bearer {accessToken}' 
--d '{'intent':'sale','payer':{'payment_method':'paypal'},'transactions':[{'amount':{'total':'7.47','currency':'USD'}}]}'<br />";
-		var_dump(shell_exec($curl));
-*/
 	}
+*/
 }
-
-
-/*
-curl -v https://api.sandbox.paypal.com/v1/payments/payment -H 'Content-Type: application/json' -H 'Authorization: Bearer {accessToken}' 
--d '{"intent":"sale","payer":{"payment_method":"paypal"},"transactions":[{"amount":{"total":"7.47","currency":"USD"}}]}'
-	*/
